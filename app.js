7a3da49c8676e66bf9577bbfbfbde46c2b0bef0c
Axel979 <83794558+Axel979@users.noreply.github.com>
2021-07-26 15:13:20 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+
+$('#navbar').load('navbar.html');
+
+const devices = [];
+
+const devices = localStorage.getItem('devices');
+const devices = localStorage.getItem('devices') || [];
+
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+      <tr>
+        <td>${device.user}</td>
+        <td>${device.name}</td>
+      </tr>`
+    );
+  });
+
+  $('#add-device').on('click', function() {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user, name });
+    console.log(devices);
+  });
+  
+devices.forEach(function(device) {
+    const table = document.querySelector('#devices');
+    const row = document.createElement('tr');
+  
+    const user = document.createElement('td');
+    const userText = document.createTextNode(device.user);
+    user.appendChild(userText);
+  
+    const name = document.createElement('td');
+    const nameText = document.createTextNode(device.name);
+    name.appendChild(nameText);
+  
+    row.appendChild(user);
+    row.appendChild(name);
+  
+    table.appendChild(row);
+  });
+
+  localStorage.setItem('devices', JSON.stringify(devices));
+  location.href = '/';
